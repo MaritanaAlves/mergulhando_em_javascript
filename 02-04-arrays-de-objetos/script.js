93fa685945aceac36d286f9bcbbdebf666ed8b9a
Maritana Alves <maritana.cz@gmail.com>
2022-10-04 12:01:30 -0300

    arrays de objetos

---
+++ 02-04-arrays-de-objetos/script.js
@@ -0,0 +1,20 @@
+var person1 = {
+  name: 'Maritana',
+  age: 23
+}
+
+var person2 = {
+  name: 'Betinho',
+  age: 21
+}
+
+var person3 = {
+  name: 'Poli',
+  age: 28
+}
+
+var list = [person1, person2, person3]
+
+for (var person of list) {
+  console.log(person)
+}
